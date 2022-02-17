c2c5d3831cb9043145e338fd9ded1e030319211b
Jules GONZALES <jules.gonzales@minet.net>
2022-02-17 12:01:15 +0100

    remove config variable "api" because not used

--- backend/config.php
+++ backend/config.php
@@ -1,5 +1,4 @@
 <?php
-$_CONFIG['api'] = apache_getenv('api');
 $_CONFIG['dbip'] = apache_getenv('dbip'); // lien vers fdpsql
 $_CONFIG['dbuser'] = apache_getenv('dbuser');
 $_CONFIG['dbpassword'] = apache_getenv('dbpassword');
