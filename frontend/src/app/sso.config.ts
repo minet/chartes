8837b4f2f9f65363e0976da7c624d8200c302da2
Jules GONZALES <jules.gonzales@minet.net>
2022-06-25 19:35:25 +0200

    change clientid for cas and add nainA integration

--- frontend/src/app/sso.config.ts
+++ frontend/src/app/sso.config.ts
@@ -3,7 +3,7 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 export const authCodeFlowConfig: AuthConfig = {
 
   redirectUri: window.location.origin,
-  clientId: 'k8s',
+  clientId: 'chartes',
   scope: 'openid profile offline_access roles',
   issuer: 'https://cas.minet.net/oidc',
   tokenEndpoint: 'https://cas.minet.net/oidc/accessToken',
