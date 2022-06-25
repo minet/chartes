f364b204adda51faac44b13adfe0364819d2ac2e
Jules GONZALES <jules.gonzales@minet.net>
2022-06-25 16:36:44 +0200

    Ajout de commentaires PHPdocs

--- backend/index.php
+++ backend/index.php
@@ -2,19 +2,25 @@
 include_once("sql.php");
 include_once("response.php");
 include_once("functions.php");
+
+// on autorise seulement le frontend à interroger le backend, et seulement pour certains types de requêtes
 header("Access-Control-Allow-Origin: ". $_CONFIG['allowed_frontend']);
 header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Authorization, Content-Type, Accept");
 header('Content-type: application/json');
 $response = new responseObject();
+
+// on récupère le header utilisé par le client.
 $headers = apache_request_headers();
 
+// cela permet de récupérer le token CAS lors de la connexion.
 $header = [
   "Authorization:". $headers['authorization']
 ];
 
 if($header) {
   $ch = curl_init();
+  // récupération des infos du client via son token cas.
   curl_setopt($ch, CURLOPT_URL,"https://cas.minet.net/oidc/profile");
   curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -23,6 +29,7 @@ if($header) {
   curl_close($ch);
 }
 
+// si le client est bien authentifié on peut autoriser les requêtes API suivantes via des variables GET.
 if(get_id()) {
   if ($_GET['getadhsigned'])
     $response->has_adh_signed();
@@ -33,19 +40,19 @@ if(get_id()) {
   else if ($_GET['getsigneddatehosting'])
     $response->get_adh_signed_date(true);
   else if ($_GET['setadhsigned'])
-      $response->set_adh_signed($_GET['language'], false, ($_GET['setadhsigned'] != "1" && is_admin() ? $_GET['setadhsigned'] : ""));
+      $response->set_adh_signed($_GET['language'], false, ($_GET['setadhsigned'] != "1" && is_admin() ? $_GET['setadhsigned'] : "")); // si on est admin on met l'ID de l'adhérent sur qui on agit, sinon 1.
   else if ($_GET['setadhsignedhosting'])
-    $response->set_adh_signed($_GET['language'], true, ($_GET['setadhsignedhosting'] != "1" && is_admin() ? $_GET['setadhsignedhosting'] : ""));
+    $response->set_adh_signed($_GET['language'], true, ($_GET['setadhsignedhosting'] != "1" && is_admin() ? $_GET['setadhsignedhosting'] : "")); // si on est admin on met l'ID de l'adhérent sur qui on agit, sinon 1.
   else if ($_GET['regeneratecharte'])
-    $response->regenerate_charte($_GET['language'], false, ($_GET['regeneratecharte'] != "1" && is_admin() ? $_GET['regeneratecharte'] : ""));
+    $response->regenerate_charte($_GET['language'], false, ($_GET['regeneratecharte'] != "1" && is_admin() ? $_GET['regeneratecharte'] : "")); // si on est admin on met l'ID de l'adhérent sur qui on agit, sinon 1.
   else if ($_GET['regeneratechartehosting'])
-    $response->regenerate_charte($_GET['language'], true, ($_GET['regeneratechartehosting'] != "1" && is_admin() ? $_GET['regeneratechartehosting'] : ""));
+    $response->regenerate_charte($_GET['language'], true, ($_GET['regeneratechartehosting'] != "1" && is_admin() ? $_GET['regeneratechartehosting'] : "")); // si on est admin on met l'ID de l'adhérent sur qui on agit, sinon 1.
   else if ($_GET['countminet'] && is_admin())
-    $response->countminet();
+    $response->countcharter(); // on ne veut que les chartes miNET
   else if ($_GET['counthosting'] && is_admin())
-    $response->counthosting();
+    $response->countcharter(true); // on ne veut que les chartes hosting
   else
     $response->error = "Bad action !";
-  echo json_encode($response, JSON_UNESCAPED_UNICODE);
+  echo json_encode($response, JSON_UNESCAPED_UNICODE); // renvoi sous format JSON la réponse de l'API.
 }
 
--- backend/response.php
+++ backend/response.php
@@ -1,97 +1,114 @@
 <?php
 
+/**
+ * Class responseObject
+ * response : message de réponse quand tout se passe bien
+ * error : message d'erreur quand un problème survient
+ */
 class responseObject {
-  public $response;
-  public $error;
+    public $response;
+    public $error;
 
-  function has_adh_signed($hosting = false, $adh = "") {
-      if (get_adh($adh)) {
-        $r = get_adh($adh);
-        if (!$hosting)
-          $this->response = ($r['datesignedminet'] ? "signé" : "non signé"); // on renvoie la valeur de la signature si le mec existe
-        else
-          $this->response = ($r['datesignedhosting'] ? "signé" : "non signé"); // on renvoie la valeur de la signature si le mec existe
-      } else
-        $this->error = "L'utilisateur n'existe pas !"; // on renvoie false si l'user n'existe pas
-  }
-
-  function get_adh_signed_date($hosting = false, $adh = "") {
-    if(get_adh($adh)) {
-      $r = get_adh($adh);
-      if(!$hosting)
-        $this->response = $r['datesignedminet']; // on renvoie la valeur de la signature si le mec existe
-      else
-        $this->response = $r['datesignedhosting']; // on renvoie la valeur de la signature si le mec existe
-    } else
-      $this->error = "L'utilisateur n'existe pas !"; // on renvoie false si l'user n'existe pas
-  }
-
-  function set_adh_signed($language = "en", $hosting = false, $adh = "") {
-    global $mysqli;
-    global $header;
-    $this->has_adh_signed($hosting, $adh);
-    if($this->response == "signé") {
-      $this->error = "Cet adhérent a déjà signé.";
-      unset($this->response);
-    } else if ($this->response == "non signé") {
-      $r = get_adh($adh);
-      if (!$hosting) {
-        $url = "https://adh6.minet.net/api/member/" . $r['id'] . "/charter/1";
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_PUT, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $content = curl_exec($ch);
-        $code = curl_getinfo($ch , CURLINFO_HTTP_CODE);
-        if ($code == 204) {
-          generate_charte($language, $hosting, $adh);
-          send_mail($language, $adh);
-        } else {
-          $this->error = "Permission insuffisante ou problème interne !";
-          unset($this->response);
-        }
-        curl_close($ch);
-      } else {
-          $url = "https://adh6.minet.net/api/member/" . $r['id'] . "/charter/2";
-          $ch = curl_init($url);
-          curl_setopt($ch, CURLOPT_PUT, true);
-          curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-          $content = curl_exec($ch);
-          $code = curl_getinfo($ch , CURLINFO_HTTP_CODE);
-          if ($code == 204) {
-              generate_charte($language, $hosting, $adh);
-              send_mail($language, $adh);
-          } else {
-              $this->error = "Permission insuffisante ou problème interne !";
-              unset($this->response);
-          }
-      }
-      $this->response = "Signature effectuée";
-    } else {
-      $this->response = "Une erreur est survenue";
+    /**
+     * Permet de vérifier si un adhérent a signé une des deux chartes
+     * @param false $hosting permet de définir si on parle de la charte hosting ou minet
+     * @param string $adh login de l'adhérent si un admin utilise la fonction sinon vide si c'est l'adhérent lui-même
+     * @return "signé" si la charte est signé sinon "non signé". Cas d'erreur : message via le champ error.
+     */
+    function has_adh_signed($hosting = false, $adh = "") {
+        // on check si l'adhérent existe (ou admin cas échéant)
+        if (get_adh($adh)) {
+            $r = get_adh($adh);
+            // on considère la charte signée si la date de signature n'est pas nulle
+            if (!$hosting)
+                $this->response = ($r['datesignedminet'] ? "signé" : "non signé"); // on renvoie la valeur de la signature si le mec existe
+            else
+                $this->response = ($r['datesignedhosting'] ? "signé" : "non signé"); // on renvoie la valeur de la signature si le mec existe
+        } else
+            $this->error = "L'utilisateur n'existe pas !"; // on renvoie false si l'user n'existe pas
     }
-  }
 
-  function regenerate_charte($language = "en", $hosting = false, $adh = "") {
-    $r = get_adh($adh);
-    if(($r['datesignedminet'] && !$hosting) || ($r['datesignedhosting'] && $hosting)) {
-      $this->response = "Charte régénérée";
-      send_mail($language, $adh, true);
-    } else
-      $this->error = "La charte n'a pas été signée ou un problème a eu lieu!";
-  }
+    /**
+     * Permet de récupérer la date de signature de l'adhérent pour l'une des deux chartes
+     * @param false $hosting permet de définir si on parle de la charte hosting ou minet
+     * @param string $adh login de l'adhérent si un admin utilise la fonction sinon vide si c'est l'adhérent lui-même
+     * @return date de signature de la charte concernée. Cas d'erreur : message via le champ error.
+     */
+    function get_adh_signed_date($hosting = false, $adh = "") {
+        if(get_adh($adh)) {
+            $r = get_adh($adh);
+            if(!$hosting)
+                $this->response = $r['datesignedminet']; // on renvoie la valeur de la signature si le mec existe
+            else
+                $this->response = $r['datesignedhosting']; // on renvoie la valeur de la signature si le mec existe
+        } else
+            $this->error = "L'utilisateur n'existe pas !"; // on renvoie false si l'user n'existe pas
+    }
 
-  function countminet() {
-    global $mysqli;
-    $result = $mysqli->query("select id from adherents where datesignedminet <> 'NULL'");
-    $this->response = mysqli_num_rows($result);
-  }
+    /**
+     * Permet de à un adhérent de signer une charte ou faire signer celle-ci si l'on est admin.
+     * @param string $language permet de définir la langue de la charte, par défaut en anglais.
+     * @param false $hosting permet de définir si on parle de la charte hosting ou minet
+     * @param string $adh login de l'adhérent si un admin utilise la fonction sinon vide si c'est l'adhérent lui-même
+     * @return Renvoie "signature effectuée" en cas de succès Cas d'erreur : message via le champ error.
+     */
+    function set_adh_signed($language = "en", $hosting = false, $adh = "") {
+        global $header;
+        $this->has_adh_signed($hosting, $adh);
+        if($this->response == "signé") {
+            $this->error = "Cet adhérent a déjà signé.";
+            unset($this->response);
+        } else if ($this->response == "non signé") {
+            $r = get_adh($adh);
+            
+            // on utilise l'api adh6 pour faire signer la charte. Id hosting : 2, Id MiNET : 1.
+            $url = "https://adh6.minet.net/api/member/" . $r['id'] . "/charter/. "($hosting ? "2" : "1");
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_PUT, true);
+            
+            // on réutilise le header pour donner à l'api ADH6 le token CAS de la personne qui souhaite signer/faire signer
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $content = curl_exec($ch);
+            $code = curl_getinfo($ch , CURLINFO_HTTP_CODE);
+            if ($code == 204) {
+                generate_charte($language, $hosting, $adh);
+                send_mail($language, $adh);
+            } else {
+                $this->error = "Permission insuffisante ou problème interne !";
+                unset($this->response);
+            }
+            curl_close($ch);
+            $this->response = "Signature effectuée";
+        } else {
+            $this->response = "Une erreur est survenue";
+        }
+    }
 
-  function counthosting() {
-    global $mysqli;
-    $result = $mysqli->query("select id from adherents where datesignedhosting <> 'NULL'");
-    $this->response = mysqli_num_rows($result);
-  }
+    /**
+     * Permet de régénérer le PDF et renvoyer un mail à l'adhérent concerné. 
+     * @param string $language langue de la charte, par défaut en anglais
+     * @param false $hosting permet de savoir de quelle charte on parle
+     * @param string $adh rempli si un admin souhaite effectuer l'action pour un adhérent, vide sinon
+     */
+    function regenerate_charte($language = "en", $hosting = false, $adh = "") {
+        $r = get_adh($adh);
+        if(($r['datesignedminet'] && !$hosting) || ($r['datesignedhosting'] && $hosting)) {
+            $this->response = "Charte régénérée";
+            
+            // la variable regenerate de la fonction permet de regénérer un PDF avant l'envoi du mail 
+            send_mail($language, $adh, true);
+        } else
+            $this->error = "La charte n'a pas été signée ou un problème a eu lieu!";
+    }
 
+    /**
+     * Permet de compter le nombre de chartes signée pour un type de charte en particulier via une requête SQL.
+     * @return le nombre de lignes obtenues dans la DB via une response.
+     */
+    function countcharter($hosting = false) {
+        global $mysqli;
+        $result = $mysqli->query("select id from adherents where " . ($hosting ? "datesignedhosting" : "datesignedminet") . " <> 'NULL'");
+        $this->response = mysqli_num_rows($result);
+    }
 }
